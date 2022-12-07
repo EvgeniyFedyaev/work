13d0517744740d3673b61559af61e80bcf8f0be3
EvgeniyFedyaev <84593861+EvgeniyFedyaev@users.noreply.github.com>
2022-12-07 23:38:50 +0300

    Update detail.php

--- page_news/local/templates/csirt_main/components/bitrix/news/news/detail.php
+++ page_news/local/templates/csirt_main/components/bitrix/news/news/detail.php
@@ -12,63 +12,169 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<?$ElementID = $APPLICATION->IncludeComponent(
-	"bitrix:news.detail",
-	"",
-	Array(
-		"DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
-		"DISPLAY_NAME" => $arParams["DISPLAY_NAME"],
-		"DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
-		"DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
-		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-		"FIELD_CODE" => $arParams["DETAIL_FIELD_CODE"],
-		"PROPERTY_CODE" => $arParams["DETAIL_PROPERTY_CODE"],
-		"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
-		"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-		"META_KEYWORDS" => $arParams["META_KEYWORDS"],
-		"META_DESCRIPTION" => $arParams["META_DESCRIPTION"],
-		"BROWSER_TITLE" => $arParams["BROWSER_TITLE"],
-		"SET_CANONICAL_URL" => $arParams["DETAIL_SET_CANONICAL_URL"],
-		"DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
-		"SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
-		"SET_TITLE" => $arParams["SET_TITLE"],
-		"MESSAGE_404" => $arParams["MESSAGE_404"],
-		"SET_STATUS_404" => $arParams["SET_STATUS_404"],
-		"SHOW_404" => $arParams["SHOW_404"],
-		"FILE_404" => $arParams["FILE_404"],
-		"INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
-		"ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
-		"ACTIVE_DATE_FORMAT" => $arParams["DETAIL_ACTIVE_DATE_FORMAT"],
-		"CACHE_TYPE" => $arParams["CACHE_TYPE"],
-		"CACHE_TIME" => $arParams["CACHE_TIME"],
-		"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
-		"USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
-		"GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
-		"DISPLAY_TOP_PAGER" => $arParams["DETAIL_DISPLAY_TOP_PAGER"],
-		"DISPLAY_BOTTOM_PAGER" => $arParams["DETAIL_DISPLAY_BOTTOM_PAGER"],
-		"PAGER_TITLE" => $arParams["DETAIL_PAGER_TITLE"],
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => $arParams["DETAIL_PAGER_TEMPLATE"],
-		"PAGER_SHOW_ALL" => $arParams["DETAIL_PAGER_SHOW_ALL"],
-		"CHECK_DATES" => $arParams["CHECK_DATES"],
-		"ELEMENT_ID" => $arResult["VARIABLES"]["ELEMENT_ID"],
-		"ELEMENT_CODE" => $arResult["VARIABLES"]["ELEMENT_CODE"],
-		"SECTION_ID" => $arResult["VARIABLES"]["SECTION_ID"],
-		"SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
-		"IBLOCK_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
-		"USE_SHARE" => $arParams["USE_SHARE"],
-		"SHARE_HIDE" => $arParams["SHARE_HIDE"],
-		"SHARE_TEMPLATE" => $arParams["SHARE_TEMPLATE"],
-		"SHARE_HANDLERS" => $arParams["SHARE_HANDLERS"],
-		"SHARE_SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
-		"SHARE_SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
-		"ADD_ELEMENT_CHAIN" => (isset($arParams["ADD_ELEMENT_CHAIN"]) ? $arParams["ADD_ELEMENT_CHAIN"] : ''),
-		'STRICT_SECTION_CHECK' => (isset($arParams['STRICT_SECTION_CHECK']) ? $arParams['STRICT_SECTION_CHECK'] : ''),
-	),
-	$component
-);?>
-<p><a href="<?=$arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"]?>"><?=GetMessage("T_NEWS_DETAIL_BACK")?></a></p>
+<section class="section main-box main-box-single">
+	<div class="container" style="position: relative;">
+		<div class="row">
+			<div class="col-12">
+				<div class="page-breadcrumbs">
+				<?$APPLICATION->IncludeComponent(
+					"bitrix:breadcrumb", 
+					"csirt_crumb", 
+					array(
+						"START_FROM" => "0",
+						"PATH" => "",
+						"SITE_ID" => "s1",
+						"COMPONENT_TEMPLATE" => "csirt_crumb"
+					),
+					false
+				);?>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<?$APPLICATION->ShowViewContent('head_news');?>
+		</div>
+		<div class="background-image">
+			<video playsinline="" autoplay="" muted="" loop="" preload="auto" pip="false" class="background-image__video">
+				<source src="/local/templates/csirt_main/video/big-main-elem.mp4" type="video/mp4">
+			</video>
+		</div>
+	</div>
+</section>
+<section class="section news-single-content">
+	<div class="container">
+		<div class="row">
+			<?$ElementID = $APPLICATION->IncludeComponent(
+				"bitrix:news.detail",
+				"news_detail",
+				Array(
+					"DISPLAY_DATE" => $arParams["DISPLAY_DATE"],
+					"DISPLAY_NAME" => $arParams["DISPLAY_NAME"],
+					"DISPLAY_PICTURE" => $arParams["DISPLAY_PICTURE"],
+					"DISPLAY_PREVIEW_TEXT" => $arParams["DISPLAY_PREVIEW_TEXT"],
+					"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+					"IBLOCK_ID" => $arParams["IBLOCK_ID"],
+					"FIELD_CODE" => $arParams["DETAIL_FIELD_CODE"],
+					"PROPERTY_CODE" => $arParams["DETAIL_PROPERTY_CODE"],
+					"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
+					"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
+					"META_KEYWORDS" => $arParams["META_KEYWORDS"],
+					"META_DESCRIPTION" => $arParams["META_DESCRIPTION"],
+					"BROWSER_TITLE" => $arParams["BROWSER_TITLE"],
+					"SET_CANONICAL_URL" => $arParams["DETAIL_SET_CANONICAL_URL"],
+					"DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
+					"SET_LAST_MODIFIED" => $arParams["SET_LAST_MODIFIED"],
+					"SET_TITLE" => $arParams["SET_TITLE"],
+					"MESSAGE_404" => $arParams["MESSAGE_404"],
+					"SET_STATUS_404" => $arParams["SET_STATUS_404"],
+					"SHOW_404" => $arParams["SHOW_404"],
+					"FILE_404" => $arParams["FILE_404"],
+					"INCLUDE_IBLOCK_INTO_CHAIN" => $arParams["INCLUDE_IBLOCK_INTO_CHAIN"],
+					"ADD_SECTIONS_CHAIN" => $arParams["ADD_SECTIONS_CHAIN"],
+					"ACTIVE_DATE_FORMAT" => $arParams["DETAIL_ACTIVE_DATE_FORMAT"],
+					"CACHE_TYPE" => $arParams["CACHE_TYPE"],
+					"CACHE_TIME" => $arParams["CACHE_TIME"],
+					"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+					"USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
+					"GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
+					"DISPLAY_TOP_PAGER" => $arParams["DETAIL_DISPLAY_TOP_PAGER"],
+					"DISPLAY_BOTTOM_PAGER" => $arParams["DETAIL_DISPLAY_BOTTOM_PAGER"],
+					"PAGER_TITLE" => $arParams["DETAIL_PAGER_TITLE"],
+					"PAGER_SHOW_ALWAYS" => "N",
+					"PAGER_TEMPLATE" => $arParams["DETAIL_PAGER_TEMPLATE"],
+					"PAGER_SHOW_ALL" => $arParams["DETAIL_PAGER_SHOW_ALL"],
+					"CHECK_DATES" => $arParams["CHECK_DATES"],
+					"ELEMENT_ID" => $arResult["VARIABLES"]["ELEMENT_ID"],
+					"ELEMENT_CODE" => $arResult["VARIABLES"]["ELEMENT_CODE"],
+					"SECTION_ID" => $arResult["VARIABLES"]["SECTION_ID"],
+					"SECTION_CODE" => $arResult["VARIABLES"]["SECTION_CODE"],
+					"IBLOCK_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["news"],
+					"USE_SHARE" => $arParams["USE_SHARE"],
+					"SHARE_HIDE" => $arParams["SHARE_HIDE"],
+					"SHARE_TEMPLATE" => $arParams["SHARE_TEMPLATE"],
+					"SHARE_HANDLERS" => $arParams["SHARE_HANDLERS"],
+					"SHARE_SHORTEN_URL_LOGIN" => $arParams["SHARE_SHORTEN_URL_LOGIN"],
+					"SHARE_SHORTEN_URL_KEY" => $arParams["SHARE_SHORTEN_URL_KEY"],
+					"ADD_ELEMENT_CHAIN" => (isset($arParams["ADD_ELEMENT_CHAIN"]) ? $arParams["ADD_ELEMENT_CHAIN"] : ''),
+					'STRICT_SECTION_CHECK' => (isset($arParams['STRICT_SECTION_CHECK']) ? $arParams['STRICT_SECTION_CHECK'] : ''),
+				),
+				$component
+			);?>
+			<div class="col-lg-4" style="z-index: 3;">
+				<?$APPLICATION->IncludeComponent(
+					"bitrix:news.list", 
+					"recommended", 
+					array(
+						"DISPLAY_DATE" => "Y",
+						"DISPLAY_NAME" => "Y",
+						"DISPLAY_PICTURE" => "Y",
+						"DISPLAY_PREVIEW_TEXT" => "Y",
+						"AJAX_MODE" => "Y",
+						"IBLOCK_TYPE" => "news",
+						"IBLOCK_ID" => "7",
+						"NEWS_COUNT" => "3",
+						"SORT_BY1" => "ACTIVE_FROM",
+						"SORT_ORDER1" => "DESC",
+						"SORT_BY2" => "SORT",
+						"SORT_ORDER2" => "ASC",
+						"FILTER_NAME" => 'arFilter',
+						"FIELD_CODE" => array(
+							0 => "ID",
+							1 => "",
+						),
+						"PROPERTY_CODE" => array(
+							0 => "",
+							1 => "DESCRIPTION",
+							2 => "",
+						),
+						"CHECK_DATES" => "Y",
+						"DETAIL_URL" => "",
+						"PREVIEW_TRUNCATE_LEN" => "",
+						"ACTIVE_DATE_FORMAT" => "d.m.Y",
+						"SET_TITLE" => "Y",
+						"SET_BROWSER_TITLE" => "Y",
+						"SET_META_KEYWORDS" => "Y",
+						"SET_META_DESCRIPTION" => "Y",
+						"SET_LAST_MODIFIED" => "Y",
+						"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+						"ADD_SECTIONS_CHAIN" => "Y",
+						"HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+						"PARENT_SECTION" => "",
+						"PARENT_SECTION_CODE" => "",
+						"INCLUDE_SUBSECTIONS" => "Y",
+						"CACHE_TYPE" => "A",
+						"CACHE_TIME" => "3600",
+						"CACHE_FILTER" => "Y",
+						"CACHE_GROUPS" => "Y",
+						"DISPLAY_TOP_PAGER" => "Y",
+						"DISPLAY_BOTTOM_PAGER" => "Y",
+						"PAGER_TITLE" => "Новости",
+						"PAGER_SHOW_ALWAYS" => "Y",
+						"PAGER_TEMPLATE" => "",
+						"PAGER_DESC_NUMBERING" => "Y",
+						"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+						"PAGER_SHOW_ALL" => "Y",
+						"PAGER_BASE_LINK_ENABLE" => "Y",
+						"SET_STATUS_404" => "Y",
+						"SHOW_404" => "Y",
+						"MESSAGE_404" => "",
+						"PAGER_BASE_LINK" => "",
+						"PAGER_PARAMS_NAME" => "arrPager",
+						"AJAX_OPTION_JUMP" => "N",
+						"AJAX_OPTION_STYLE" => "Y",
+						"AJAX_OPTION_HISTORY" => "N",
+						"AJAX_OPTION_ADDITIONAL" => "",
+						"COMPONENT_TEMPLATE" => ".default",
+						"STRICT_SECTION_CHECK" => "N",
+						"FILE_404" => ""
+					),
+					false
+				);?>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?if($arParams["USE_RATING"]=="Y" && $ElementID):?>
 <?$APPLICATION->IncludeComponent(
 	"bitrix:iblock.vote",
